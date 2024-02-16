494a8fca3c91b3d5e322c577b84718a23809b7b9
Greene, Ava <greeava@iu.edu>
2024-02-16 17:43:24 -0500

    new files

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HomeScreen = () => {
+    return (
+        <div>
+            <p>Home Screen</p>
+            
+        </div>
+    )
+}
+
+export default HomeScreen
+
