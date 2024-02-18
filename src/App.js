9e7dfe530ced8a6ac8d60d0e1de19bc801430da4
Sampath Kumar, Yashaswini <ysampath@iu.edu>
2024-02-17 21:23:22 -0500

    Login and signUp page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+// import './App.css';
+// import HomeScreen from './screens/HomeScreen';
+// import SignIn from './screens/SignIn'
+// import SignUp from './screens/SignUp'
+// import AppNav from './AppNav';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <HomeScreen />
+//       <SignIn />
+//       <SignUp />
+//     </div>
+//   );
+// }
+
+// export default App;
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
+import SignIn from './screens/SignIn';
+import SignUp from './screens/SignUp';
+import AppNav from './AppNav';
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen />
+      <Router>
+        <AppNav />
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import { AppBar, Toolbar, Typography, Button, Container, TextField } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const HomeScreen = () => {
     return (
         <div>
-            <p>Home Screen</p>
-            
-        </div>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            EzTravel
+          </Typography>
+          <Button color="inherit" component={Link} to="/SignIn">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="md" sx={{ mt: 2 }}>
+        <Typography variant="h4" gutterBottom>
+          Destination
+        </Typography>
+        <TextField label="Search" variant="outlined" fullWidth />
+      </Container>
+    </div>
     )
 }
 
