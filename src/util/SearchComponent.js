884158bb6c1b0cc480c6a6c134c4d10240afd7ca
Greene, Ava <greeava@iu.edu>
2024-02-25 14:03:28 -0500

    fixed search position

--- src/util/SearchComponent.js
+++ src/util/SearchComponent.js
@@ -3,13 +3,13 @@ import { InputBase, IconButton, Paper, dividerClasses } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 function SearchComponent() {
     return (
-        <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center',  height: '100vh', width: '100%', marginbottom:'100px'}}>
+        <div style={{display: 'block', justifyContent: 'center', alignItems: 'center',  height: '100vh', width: '50%', margin: 'auto', marginbottom:'100px'}}>
             <Paper component="form" style={{ display: 'block', width: '100%', padding: '5px 10px', margin: '10px 10px',border: '1px solid #2484BF'  }}>
                 <InputBase
                     placeholder="Search"
                     inputProps={{ 'aria-label': 'search' }}
                 />
-                <IconButton type="submit" aria-label="search" style={{ color: '#2484BF', padding: '10px', justifyContent:'right' }}>
+                <IconButton type="submit" aria-label="search" style={{color: '#2484BF', padding: '10px', justifyContent:'right' }}>
                     <SearchIcon />
                 </IconButton>
             </Paper>
