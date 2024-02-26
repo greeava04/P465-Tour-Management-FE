b60a10228eb3523043a28a5889f4cafb2d0c96dd
Greene, Ava <greeava@iu.edu>
2024-02-26 13:46:48 -0500

    Search bar updates

--- src/util/SearchComponent.js
+++ src/util/SearchComponent.js
@@ -3,15 +3,16 @@ import { InputBase, IconButton, Paper, dividerClasses } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
 function SearchComponent() {
     return (
-        <div style={{display: 'block', justifyContent: 'center', alignItems: 'center',  height: '100vh', width: '50%', margin: 'auto', marginbottom:'100px'}}>
-            <Paper component="form" style={{ display: 'block', width: '100%', padding: '5px 10px', margin: '10px 10px',border: '1px solid #2484BF'  }}>
-                <InputBase
-                    placeholder="Search"
-                    inputProps={{ 'aria-label': 'search' }}
-                />
-                <IconButton type="submit" aria-label="search" style={{color: '#2484BF', padding: '10px', justifyContent:'right' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Paper component="form" style={{ display: 'flex', alignItems: 'center', width: '100%', padding: '2px 4px', margin: '0 auto', border: '1px solid #2484BF' }}>
+                <IconButton type="submit" aria-label="search" style={{ color: '#2484BF', padding: '10px' }}>
                     <SearchIcon />
                 </IconButton>
+                <InputBase
+                    placeholder="Search"
+                    style={{ marginLeft: '10px', flex: 1 }}
+                    inputProps={{ 'aria-label': 'search' }}
+                />
             </Paper>
         </div>
     )
