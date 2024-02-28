de3728eb22f6b37d5e362b5809fe3a3488134a09
Owen Harris <owenh0410@gmail.com>
2024-02-28 20:27:28 +0000

    Made changes to HomeScreen ready for backend connection

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,26 +2,29 @@ import React, { useEffect, useState, image } from 'react'
 import AppBarComponent from '../util/AppBarComponent'
 import EZTravelLogo from '../images/EZTravelLogo.png'
 import SearchComponent from '../util/SearchComponent'
+import Container from '@mui/material/Container';
 
 
 const HomeScreen = () => {
-  const [locations, setLocations] = useState([]);
-  const [userSelectedLocationsetUserSelectedLocation] = useState();
+  const [locations, setLocations] = useState();
+  const [userSelectedLocation, setUserSelectedLocation] = useState();
 
   //We need to map each location to a grid item, rn it's hard coded, this is just to get us ready
-  // useEffect(
-  //   () => {
-  //     fetch('http://localhost:3000/getRandPlace')
-  //     .then(response => response.json())
-  //     .then(data => setLocations(data))
-  //   }, []
-  // )
+  useEffect(
+    () => {
+      // fetch('http://localhost:3000/getRandPlace')
+      // .then(response => response.json())
+      // .then(data => setLocations(data))
+      setLocations([{image : EZTravelLogo, name: "Location Name", rating: "★★★★☆", price: "20"}])
+    }, []
+  )
 
 
 
     return (
         <div class="container">
             <AppBarComponent />
+            <Container maxWidth="xl">
             <div class="sidebar">
       {/* We should make components for filter and search like appbarcomponent */}
               <div class ="filters"></div>
@@ -32,57 +35,30 @@ const HomeScreen = () => {
             </div>
       {/* We will need to make this dynamic, taking from the locations array to only have a map that it populates */}
             <div class="grid-container">
-              <div class="grid-item">
-                <img src={EZTravelLogo} alt="Location Image" />
-                 <div class="grid-text">
-                <h3>Location Name</h3>
-                <p>Price: $XX</p>
-                <p>Rating: ★★★★☆</p>
-                </div>
-              </div>
-              <div class="grid-item">
-                <img src={EZTravelLogo} alt="Location Image" />
-                <div class="grid-text">
-                <h3>Location Name</h3>
-                <p>Price: $XX</p>
-                <p>Rating: ★★★★☆</p>
-                </div>
-              </div>
-              <div class="grid-item">
-                <img src={EZTravelLogo} alt="Location Image" />
-                <div class="grid-text">
-                <h3>Location Name</h3>
-                <p>Price: $XX</p>
-                <p>Rating: ★★★★☆</p>
-                </div>
-              </div>
-              <div class="grid-item">
-                <img src={EZTravelLogo} alt="Location Image" />
-                <div class="grid-text">
-                <h3>Location Name</h3>
-                <p>Price: $XX</p>
-                <p>Rating: ★★★★☆</p>
-                </div>
-              </div>
-              <div class="grid-item">
-                <img src={EZTravelLogo} alt="Location Image" />
-                <div class="grid-text">
-                <h3>Location Name</h3>
-                <p>Price: $XX</p>
-                <p>Rating: ★★★★☆</p>
-                </div>
-              </div>
-              <div class="grid-item">
-                <img src={EZTravelLogo} alt="Location Image" />
-                <div class="grid-text">
-                <h3>Location Name</h3>
-                <p>Price: $XX</p>
-                <p>Rating: ★★★★☆</p>
-                </div>
-              </div>
+              <Place place={{image : EZTravelLogo, name: "Location Name", rating: "★★★★☆", price: "20"}}></Place>
+              {
+                locations &&
+                locations.map((place) => {
+                  return <Place place={place}></Place>
+                })
+              }
             </div>
+            </Container>
         </div>
     )
 }
+
+const Place = (props) => {
+  return (
+    <div class="grid-item">
+      <img src={props.place.image} alt={props.place.name} />
+       <div class="grid-text">
+      <h3>{props.place.name}</h3>
+      <p>Price: {props.place.price}</p>
+      <p>Rating: {props.place.rating}</p>
+      </div>
+    </div>
+  )
+};
 
 export default HomeScreen
