df2ba782f936aebe5ef4da2d74b1425212f8d9cb
Owen Harris <owenh0410@gmail.com>
2024-03-24 19:49:46 +0000

    Updated Itinerary, Itinerary sharing, Currency Conversion, and homescreen

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import SignIn from './screens/SignIn';
 import SignUp from './screens/SignUp';
 import AppNav from './AppNav';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 function App() {
+  // Lets handle all navigation in app nav - we will not need to touch this file until we need to deal with protected routes, but we'll be able to prob do that in appnav
   return (
-    //Lets handle all navigation in app nav - we will not need to touch this file until we need to deal with protected routes, but we'll be able to prob do that in appnav
-    <Router>
-      <div className="">
-        <AppNav />
-      </div>
-    </Router>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Router>
+        <div className="">
+          <AppNav />
+        </div>
+      </Router>
+    </LocalizationProvider>
   );
 }
 
--- src/util/SearchComponent.js
+++ src/util/SearchComponent.js
@@ -1,18 +1,86 @@
 import React from 'react';
 import { InputBase, IconButton, Paper, dividerClasses } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
-function SearchComponent() {
+function SearchComponent({ setLocations }) {
+    const [items, setItems] = React.useState([]);
+
+    const keyHandler = (event) => {
+        // console.log(event.key)
+        let input = document.getElementById("searchBox");
+        let popup = document.querySelector('.popup')
+
+        if (event.key == "Backspace" && input.value.length <= 1) {
+            setItems([]);
+            popup.classList.remove("focus")
+        } else {
+        fetch(`http://10.1.1.111:3001/place/search?q=${input.value}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data, data.places)
+                setItems(data.places)
+                popup.classList.add("focus")
+            })
+            .catch(error => console.log('Error fetching locations:', error));
+        }
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        let input = document.getElementById("searchBox");
+        document.querySelector('.popup').classList.remove('focus')
+        if (input.value == "") {
+            fetch('http://10.1.1.111:3001/places')
+            .then(response => response.json())
+            .then(data => setLocations(data.places))
+            .catch(error => console.log('Error fetching locations:', error));
+        } else {
+            fetch(`http://10.1.1.111:3001/place/search?q=${input.value}`)
+            .then(response => response.json())
+            .then(data => setLocations(data.places))
+            .catch(error => console.log('Error fetching locations:', error));
+        }
+
+
+    }
+
+    const autoClick = (name) => {
+        let input = document.getElementById("searchBox");
+        document.querySelector('.popup').classList.remove('focus')
+        input.value = "";
+            fetch(`http://10.1.1.111:3001/place/search?q=${name}`)
+            .then(response => response.json())
+            .then(data => setLocations(data.places))
+            .catch(error => console.log('Error fetching locations:', error));
+    }
+
+    let itemsRender = items.map((place) => {
+        return (
+            <div class="item" onClick={(event) => autoClick(place.name)}>{place.name}</div>
+        )
+    })
+
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <Paper component="form" style={{ display: 'flex', alignItems: 'center', width: '100%', padding: '2px 4px', margin: '0 auto', border: '1px solid #2484BF' }}>
+            <Paper component="form" onSubmit={handleSubmit} style={{ display: 'flex', alignItems: 'center', width: '100%', padding: '2px 4px', margin: '0 auto', border: '1px solid #2484BF' }}>
+            <div class="box">
                 <IconButton type="submit" aria-label="search" style={{ color: '#2484BF', padding: '10px' }}>
                     <SearchIcon />
                 </IconButton>
+                
                 <InputBase
                     placeholder="Search"
                     style={{ marginLeft: '10px', flex: 1 }}
                     inputProps={{ 'aria-label': 'search' }}
-                />
+                    id = "searchBox"
+                    onKeyDown={keyHandler}
+                    autoComplete='off'
+                >
+                </InputBase>
+                <div class="popup">
+                    {itemsRender}
+                </div>
+                </div>
+                
             </Paper>
         </div>
     )
